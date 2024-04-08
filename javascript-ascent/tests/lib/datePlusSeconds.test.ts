f584be577acbc7ccbbf9ddb7d4df6acb4c4d5802
Oliver Forral <intrepion@gmail.com>
2024-04-07 19:14:00 -0600

    datePlusSeconds - should give date plus zero seconds - red

---
+++ javascript-ascent/tests/lib/datePlusSeconds.test.ts
@@ -0,0 +1,11 @@
+describe("datePlusSeconds", () => {
+  it("should give date plus zero seconds", () => {
+    const someDate = Date.UTC(0);
+    const seconds = 0;
+    const expected = Date.UTC(0);
+
+    const actual = datePlusSeconds(someDate, seconds);
+
+    expect(actual).toEqual(expected);
+  });
+});
