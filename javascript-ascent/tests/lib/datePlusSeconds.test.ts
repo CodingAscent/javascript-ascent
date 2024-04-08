5f6b5db5a563d3ce4377bc2f05ec0886aaaff5bc
Oliver Forral <intrepion@gmail.com>
2024-04-08 16:24:48 -0600

    datePlusSeconds - should give date plus 1 second - red

--- javascript-ascent/tests/lib/datePlusSeconds.test.ts
+++ javascript-ascent/tests/lib/datePlusSeconds.test.ts
@@ -10,4 +10,14 @@ describe("datePlusSeconds", () => {
 
     expect(actual).toEqual(expected);
   });
+
+  it("should give date plus 1 second", () => {
+    const someDate = Date.UTC(0);
+    const seconds = 1;
+    const expected = Date.UTC(0) + 1000;
+
+    const actual = datePlusSeconds(someDate, seconds);
+
+    expect(actual).toEqual(expected);
+  });
 });
