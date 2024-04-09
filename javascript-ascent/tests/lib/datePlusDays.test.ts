e895bd7441ad8725673b3b326dea76f71ebfcc2c
Oliver Forral <intrepion@gmail.com>
2024-04-08 23:55:38 -0600

    datePlusDays - should give date plus one day - red

--- javascript-ascent/tests/lib/datePlusDays.test.ts
+++ javascript-ascent/tests/lib/datePlusDays.test.ts
@@ -10,4 +10,14 @@ describe("datePlusDays", () => {
 
     expect(actual).toEqual(expected);
   });
+
+  it("should give date plus one day", () => {
+    const someDate = Date.UTC(0);
+    const days = 1;
+    const expected = Date.UTC(0) + 24 * 60 * 60 * 1000;
+
+    const actual = datePlusDays(someDate, days);
+
+    expect(actual).toEqual(expected);
+  });
 });
