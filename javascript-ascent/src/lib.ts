9410caa70efb9e1fe7291b09c1f8432dc58ca64a
Oliver Forral <intrepion@gmail.com>
2024-04-08 23:04:00 -0600

    datePlusMinutes - should give date plus zero minutes - green

--- javascript-ascent/src/lib.ts
+++ javascript-ascent/src/lib.ts
@@ -1,3 +1,7 @@
 export function datePlusSeconds(date: number, seconds: number) {
   return date + seconds * 1000;
 }
+
+export function datePlusMinutes(date: number, minutes: number) {
+  return date;
+}
