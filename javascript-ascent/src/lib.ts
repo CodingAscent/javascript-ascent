7c996f32f6c3d2b5a958f0d4e62f970cf0324369
Oliver Forral <intrepion@gmail.com>
2024-04-08 23:26:48 -0600

    datePlusHours - should give date plus zero hours - green

--- javascript-ascent/src/lib.ts
+++ javascript-ascent/src/lib.ts
@@ -1,7 +1,11 @@
-export function datePlusSeconds(date: number, seconds: number) {
-  return date + seconds * 1000;
+export function datePlusHours(date: number, hours: number) {
+  return date;
 }
 
 export function datePlusMinutes(date: number, minutes: number) {
   return date + minutes * 60 * 1000;
+}
+
+export function datePlusSeconds(date: number, seconds: number) {
+  return date + seconds * 1000;
 }
