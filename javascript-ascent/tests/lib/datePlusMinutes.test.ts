e209a2e9c19aab7891755e0bd1a6be8ee3a6189d
Oliver Forral <intrepion@gmail.com>
2024-04-08 23:05:33 -0600

    datePlusMinutes - should give date plus one minute - red

--- javascript-ascent/tests/lib/datePlusMinutes.test.ts
+++ javascript-ascent/tests/lib/datePlusMinutes.test.ts
@@ -10,4 +10,14 @@ describe("datePlusMinutes", () => {
 
     expect(actual).toEqual(expected);
   });
+
+  it("should give date plus 1 minute", () => {
+    const someDate = Date.UTC(0);
+    const minutes = 1;
+    const expected = Date.UTC(0) + 60000;
+
+    const actual = datePlusMinutes(someDate, minutes);
+
+    expect(actual).toEqual(expected);
+  });
 });
